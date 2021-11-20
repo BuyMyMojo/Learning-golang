343a48c2959cd7be9bef8aac2be385dce4dbc71a
Owen Quinlan <46948241+BuyMyMojo@users.noreply.github.com>
2021-11-20 21:58:21 +1100

    Made the bild test also a multi threaded test.
    it processes 5 images at once instead of just one. this is obviously static and not useful at all but it's an interesting start

--- bild test/bild.go
+++ bild test/bild.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/anthonynsimon/bild/blur"
 	"github.com/anthonynsimon/bild/imgio"
+	"sync"
 )
 
 func main() {
-	img, err := imgio.Open("pakemon2.png")
+	var wg sync.WaitGroup
+	wg.Add(5)
+
+
+	go func() {
+		blurImg("./pakemon1.png", "./pakemon1-blur.png")
+		wg.Done()
+	}()
+
+	go func() {
+		blurImg("./pakemon2.png", "./pakemon2-blur.png")
+		wg.Done()
+	}()
+
+	go func() {
+		blurImg("./pakemon3.png", "./pakemon3-blur.png")
+		wg.Done()
+	}()
+
+	go func() {
+		blurImg("./pakemon4.png", "./pakemon4-blur.png")
+		wg.Done()
+	}()
+
+	go func() {
+		blurImg("./pakemon5.png", "./pakemon5-blur.png")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+// blur images
+func blurImg(source string, target string) {
+	img, err := imgio.Open(source)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -16,8 +50,9 @@ func main() {
 
 	result := blur.Gaussian(img, 128.0)
 
-	if err := imgio.Save("pakemon2_edited.png", result, imgio.PNGEncoder()); err != nil {
+	if err := imgio.Save(target, result, imgio.PNGEncoder()); err != nil {
 		fmt.Println(err)
 		return
 	}
+
 }
